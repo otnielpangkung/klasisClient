383ce083489447c475148ad292a45bce4fd3140a
otnielpangkung <otnielpangkung@yahoo.co.id>
2022-02-04 10:21:45 +0800

    4feb

--- test.vue
+++ test.vue
@@ -1,25 +1,13 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <router-view />
-  </div>
+  
 </template>
+
 <script>
-import Navbar from "./components/Navbar";
 export default {
-  components: {
-    Navbar
-  }
-};
-</script>
-<style>
-#app {
-  font-family: "Roboto Condensed", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  padding: 0;
-  color: #2c3e50;
-  min-height: 100vh;
+
 }
+</script>
+
+<style>
+
 </style>
